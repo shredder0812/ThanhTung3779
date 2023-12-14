f98858ca6ac41be14e94247fe9b8db90cc8dca85
tungct <tungct08122002@gmail.com>
2023-12-14 18:07:23 +0700

    sua xong prj2

--- Project2/src/userprog/syscall.h
+++
@@ -1,9 +0,0 @@
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-/* My code here */
-typedef int pid_t;
-struct lock file_lock;
-void syscall_init (void);
-void exit(int);
-/*== My code here */
-#endif /* userprog/syscall.h */
